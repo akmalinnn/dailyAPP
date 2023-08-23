5ad7af7d19a1343e3420bb715815ff0f0cf37691
dxdiaga <akmalnafis25@gmail.com>
2023-08-23 11:17:31 +0700

    edited css and item

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const addForm = document.querySelector('.add-input');
-const list = document.querySelector('.list-group');
-const search = document.querySelector('.custom-input');
-const noItemsMessage = document.querySelector('.no-items-message');
+const addForm = document.querySelector(".add-input");
+const list = document.querySelector(".list-group");
+const search = document.querySelector(".custom-input");
+const noItemsMessage = document.querySelector(".no-items-message");
 
 const generateTemplate = todo => {
-    const listItem = document.createElement('li');
-    listItem.className = 'list-item';
+    const listItem = document.createElement("li");
+    listItem.className = "list-item";
     listItem.innerHTML = `
         <span>${todo}</span>
         <i class="far fa-trash-alt delet"></i>
@@ -13,43 +13,44 @@ const generateTemplate = todo => {
     list.insertBefore(listItem, list.firstChild); 
 };
 
-addForm.addEventListener('submit', e => {
+addForm.addEventListener("submit", e => {
     e.preventDefault();
     const todo = addForm.add.value.trim();
 
     if (todo.length) {
         generateTemplate(todo);
         addForm.reset();
+        saveData();
         updateNoItemsMessage();
     }
 });
 
-list.addEventListener('click', e => {
-    if (e.target.classList.contains('delet')) {
+
+list.addEventListener("click", e => {
+    if (e.target.classList.contains("delet")) {
         e.target.parentElement.remove();
         updateNoItemsMessage(); // Call the function after removing an item
-    } else if (e.target.tagName ==='SPAN') {
-        e.target.classList.toggle('completed');
+    } else if (e.target.tagName ==="SPAN") {
+        e.target.classList.toggle("completed");
 
-        const completedItems = list.querySelectorAll('.completed');
-        const notCompletedItems = list.querySelectorAll(':not(.completed)');
+        const completedItems = list.querySelectorAll(".completed");
+        const notCompletedItems = list.querySelectorAll(":not(.completed)");
             
         completedItems.forEach(item => list.appendChild(item.parentNode));
         notCompletedItems.forEach(item => list.appendChild(item.parentNode));
-        
-
     }
+    saveData();
 });
 
 const updateNoItemsMessage = () => {
-const listItems = list.querySelectorAll('.list-item');
+const listItems = list.querySelectorAll(".list-item");
     
     if (listItems.length === 0) {
-        noItemsMessage.style.display = 'block';
-        search.style.display = 'none';
+        noItemsMessage.style.display = "block";
+        search.style.display = "none";
     } else {
-        noItemsMessage.style.display = 'none';
-        search.style.display = 'block';
+        noItemsMessage.style.display = "none";
+        search.style.display = "block";
     }
 };
 
@@ -57,53 +58,52 @@ const filterTodos = term => {
     const listItems = Array.from(list.children);
     
     listItems.forEach(item => {
-        const todoText = item.querySelector('span').textContent.toLowerCase();
+        const todoText = item.querySelector("span").textContent.toLowerCase();
         if (todoText.includes(term)) {
-            item.classList.remove('hidden'); 
+            item.classList.remove("hidden"); 
         } else {
-            item.classList.add('hidden'); 
+            item.classList.add("hidden"); 
         }
-        
     });
 
      
 };
 
-search.addEventListener('keyup', e => {
+search.addEventListener("keyup", e => {
     const term = search.value.trim().toLowerCase();
     filterTodos(term);
 
-    if (term === '') {
+    if (term === "") {
         Array.from(list.children).forEach(item => {
-            item.classList.remove('hidden');
+            item.classList.remove("hidden");
         });
     }
 });
 
 function updateClock() {
     let today = new Date();
-    let time = today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false });
+    let time = today.toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric", hour12: false });
     let hours = today.getHours();
-    let dayOfWeek = today.toLocaleString('en-US', { weekday: 'long' });
+    let dayOfWeek = today.toLocaleString("en-US", { weekday: "long" });
     let day = today.getDate();
     let month = today.getMonth();
     let year = today.getFullYear();
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    document.getElementById('time').innerHTML = time;
-    document.getElementById('date').innerHTML = dayOfWeek + ", <br>" + day + " " + months[month] + " " + year;
-    document.getElementById('greeting').innerHTML = displayGreeting(hours);
+    document.getElementById("time").innerHTML = time;
+    document.getElementById("date").innerHTML = dayOfWeek + ", <br>" + day + " " + months[month] + " " + year;
+    document.getElementById("greeting").innerHTML = displayGreeting(hours);
 
-    requestAnimationFrame(updateClock); // Call updateClock again before the next frame
+    requestAnimationFrame(updateClock); 
 }
 
 function displayGreeting(hours) {
   if (hours < 11) {
-    return 'Good morning';
+    return "Good morning";
   } else if (hours > 17) {
-    return 'Good evening';
+    return "Good evening";
   } else {
-    return 'Good afternoon';
+    return "Good afternoon";
   }
 }
 
@@ -112,30 +112,48 @@ const currentTime = new Date().getHours();
 
 function init_auto(){
     if ((currentTime > 23) || (currentTime < 4)){
-        body.classList.add('midnight');
+        body.classList.add("midnight");
     }
     else if(currentTime < 6){
-        body.classList.add('dawn');
+        body.classList.add("dawn");
     }
     else if(currentTime < 9){
-        body.classList.add('morning');
+        body.classList.add("morning");
     }
     else if(currentTime < 17){
-        body.classList.add('noon');
+        body.classList.add("noon");
     }
     else if(currentTime < 18){
-        body.classList.add('afternoon');
+        body.classList.add("afternoon");
     }
     else if(currentTime < 19){
-        body.classList.add('sunset');
+        body.classList.add("sunset");
     }
     else if(currentTime < 23){
-        body.classList.add('evening');
+        body.classList.add("evening");
     }
     else{
-        body.classList.add('midnight');
+        body.classList.add("midnight");
     }
 }
-
 init_auto();
 updateClock();
+
+function saveData(){
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showTask(){
+    const storedData = localStorage.getItem("data");
+    list.innerHTML = localStorage.getItem("data");
+    if (storedData === null || storedData === "") {
+        updateNoItemsMessage();
+    }
+}
+showTask();
+
+let alerted = localStorage.getItem('alerted') || '';
+if (alerted != 'yes') {
+ alert("Better in fullscreen");
+ localStorage.setItem('alerted','yes');
+}
